addfd7e15a9e21d4db859e5932cd29ed726c320f
Akshay <akshaykumar45349@gmail.com>
2023-06-26 10:47:16 +0530

    fixed navbar issues

--- pages/component/navbar.js
+++ pages/component/navbar.js
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react'
 import {  useSession, signOut } from 'next-auth/react';
 import { Navbar, Nav } from 'rsuite';
@@ -5,28 +7,36 @@ import HomeIcon from '@rsuite/icons/legacy/Home';
 import 'rsuite/dist/rsuite-no-reset.min.css';
 
 
+
 const HomeNavbar = () => {
 
     const {data:session}=useSession();
     const handleLogout=async(e)=>{
+      e.preventDefault();
       await signOut({redirect:false,callbackUrl:'/login'});
       
     }
-    
+  if(!session){
+    return (
+      <>
+      <div>No-session</div>
+      </>
+    )
+  } 
   return (
     <Navbar appearance="inverse" className='shadow-xl sticky z-50 top-0'>
     <Navbar.Brand className='font-pattya text-3xl'>Aka Notes</Navbar.Brand>
     <Nav>
-      <Nav.Item icon={<HomeIcon />}>Home</Nav.Item>
-      <Nav.Item> About</Nav.Item>  
-      <Nav.Item>Contact</Nav.Item>  
-      <Nav.Item>Enjoy</Nav.Item>
+      <Nav.Item href='#' icon={<HomeIcon />}>Home</Nav.Item>
+      <Nav.Item href='#' > About</Nav.Item>  
+      <Nav.Item href='#' >Contact</Nav.Item>  
+      <Nav.Item href='#' >Enjoy</Nav.Item>
     </Nav>
     
     <Nav pullRight>
       <Nav.Menu placement='bottomEnd' trigger="hover"  title={session.token.name[0].toUpperCase()} className='bg-green-400 font-semibold text-2xl rounded-full'>
-        <Nav.Item>{session.token.email}</Nav.Item>
-        <Nav.Item><button onClick={handleLogout}>Logout</button></Nav.Item>
+        <Nav.Item href='#' >{session.token.email}</Nav.Item>
+        <Nav.Item href='#' ><button onClick={handleLogout}>Logout</button></Nav.Item>
       </Nav.Menu>
     </Nav>
   </Navbar>
